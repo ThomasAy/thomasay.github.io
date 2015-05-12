ec27398fa3909a606bc9c7f13157617aa441cd5a
ThomasAy <thomas@phonations.com>
2015-05-12 23:30:04 +0200

    changement du carousel pour Lazy

--- Mathilde/carousel.js
+++ Mathilde/carousel.js
@@ -2,11 +2,9 @@ $(document).ready(function() {
 
   $("#owl-demo").owlCarousel({
 
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
-
-      items : 4,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
+    items : 3,
+    lazyLoad : true,
+    navigation : true
 
   });
 
